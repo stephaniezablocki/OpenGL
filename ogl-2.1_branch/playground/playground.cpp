9afa426b3c8061139e46880036d9faccb445ff8e
Stephanie Zablocki <stephaniezablocki@gmail.com>
2021-10-11 13:48:19 -0400

    Red triangle working

--- ogl-2.1_branch/playground/playground.cpp
+++ ogl-2.1_branch/playground/playground.cpp
@@ -2,14 +2,16 @@
 #include <stdlib.h>
 
 #include <GL/glew.h>
-
 #include <glfw3.h>
-
 #include <glm/glm.hpp>
+
+#include <common/shader.hpp>
 using namespace glm;
 
-int main( void )
+
+int main(void)
 {
+    
 	// Initialise GLFW
     glewExperimental = true;
 	if( !glfwInit() )
@@ -34,6 +36,7 @@ int main( void )
 		return -1;
 	}
     glfwMakeContextCurrent(window);
+    
     glewExperimental=true;
     if(glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
@@ -43,11 +46,38 @@ int main( void )
     //Set escape key input
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
     
+    
+    GLuint programID = LoadShaders("SimpleVertexShader.vertexshader", "SimpleFragmentShader.fragmentshader");
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(programID);
+    
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+    
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+    };
+    
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    
     do{
         //Clear the screen
         glClear(GL_COLOR_BUFFER_BIT);
         
-        //Draw nothing
+        //1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                              0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
         
         //Swap buffers
         glfwSwapBuffers(window);
