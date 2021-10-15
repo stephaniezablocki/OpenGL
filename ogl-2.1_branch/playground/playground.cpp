7cb44712f58fc72219a9d83c009ccf49ba8d9a0d
Stephanie Zablocki <stephaniezablocki@gmail.com>
2021-10-15 10:52:40 -0400

    Scroll working but cube starts off-screen

--- ogl-2.1_branch/playground/playground.cpp
+++ ogl-2.1_branch/playground/playground.cpp
@@ -7,6 +7,7 @@
 
 #include <common/shader.hpp>
 #include <common/texture.hpp>
+#include <common/controls.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
 #include <glm/gtx/euler_angles.hpp>
@@ -15,6 +16,7 @@
 
 using namespace glm;
 
+GLFWwindow* window;
 
 int main(void)
 {
@@ -35,7 +37,6 @@ int main(void)
 
 
     // Open a window and create its OpenGL context
-    GLFWwindow* window;
 	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
@@ -52,6 +53,7 @@ int main(void)
     
     //Set escape key input
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     
     // Dark blue background
     glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
@@ -65,22 +67,22 @@ int main(void)
 
     
     //Projection matrix: 45˚ field of view, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    int width = 4;
-    int height = 3;
+    //int width = 4;
+    //int height = 3;
     //glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float)width/(float)height, 0.1f, 100.0f);
-    glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f);
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f);
     
     //Camera matrix
-    glm::mat4 View = glm::lookAt(
-                                 glm::vec3(4,3,3), glm::vec3(0,0,0), glm::vec3(0,1,0));
+    //glm::mat4 View = glm::lookAt(
+    //                             glm::vec3(4,3,3), glm::vec3(0,0,0), glm::vec3(0,1,0));
     
     /*glm::mat4 translationMatrix = glm::translate(glm::mat4(), glm::vec3(2.0f, 0.0f, 0.0f));
     glm::mat4 rotationMatrix = eulerAngleXYZ(0.0f,90.0f,0.0f);
     glm::mat4 scalingMatrix = glm::scale(glm::vec3(4.0f, 4.0f, 4.0f));
     
     glm::mat4 Model = rotationMatrix * translationMatrix * scalingMatrix * glm::mat4(1.0f);*/
-    glm::mat4 Model = glm::mat4(1.0f);
-    glm::mat4 mvp = Projection * View * Model;
+    //glm::mat4 Model = glm::mat4(1.0f);
+    //glm::mat4 mvp = Projection * View * Model;
     
     GLuint MatrixID = glGetUniformLocation(programID, "MVP");
     
@@ -134,42 +136,42 @@ int main(void)
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
     
     static const GLfloat g_uv_buffer_data[] = {
-        0.000059f, 1.0f-0.000004f,
-        0.000103f, 1.0f-0.336048f,
-        0.335973f, 1.0f-0.335903f,
-        1.000023f, 1.0f-0.000013f,
-        0.667979f, 1.0f-0.335851f,
-        0.999958f, 1.0f-0.336064f,
-        0.667979f, 1.0f-0.335851f,
-        0.336024f, 1.0f-0.671877f,
-        0.667969f, 1.0f-0.671889f,
-        1.000023f, 1.0f-0.000013f,
-        0.668104f, 1.0f-0.000013f,
-        0.667979f, 1.0f-0.335851f,
-        0.000059f, 1.0f-0.000004f,
-        0.335973f, 1.0f-0.335903f,
-        0.336098f, 1.0f-0.000071f,
-        0.667979f, 1.0f-0.335851f,
-        0.335973f, 1.0f-0.335903f,
-        0.336024f, 1.0f-0.671877f,
-        1.000004f, 1.0f-0.671847f,
-        0.999958f, 1.0f-0.336064f,
-        0.667979f, 1.0f-0.335851f,
-        0.668104f, 1.0f-0.000013f,
-        0.335973f, 1.0f-0.335903f,
-        0.667979f, 1.0f-0.335851f,
-        0.335973f, 1.0f-0.335903f,
-        0.668104f, 1.0f-0.000013f,
-        0.336098f, 1.0f-0.000071f,
-        0.000103f, 1.0f-0.336048f,
-        0.000004f, 1.0f-0.671870f,
-        0.336024f, 1.0f-0.671877f,
-        0.000103f, 1.0f-0.336048f,
-        0.336024f, 1.0f-0.671877f,
-        0.335973f, 1.0f-0.335903f,
-        0.667969f, 1.0f-0.671889f,
-        1.000004f, 1.0f-0.671847f,
-        0.667979f, 1.0f-0.335851f
+        0.000059f, 0.000004f,
+        0.000103f, 0.336048f,
+        0.335973f, 0.335903f,
+        1.000023f, 0.000013f,
+        0.667979f, 0.335851f,
+        0.999958f, 0.336064f,
+        0.667979f, 0.335851f,
+        0.336024f, 0.671877f,
+        0.667969f, 0.671889f,
+        1.000023f, 0.000013f,
+        0.668104f, 0.000013f,
+        0.667979f, 0.335851f,
+        0.000059f, 0.000004f,
+        0.335973f, 0.335903f,
+        0.336098f, 0.000071f,
+        0.667979f, 0.335851f,
+        0.335973f, 0.335903f,
+        0.336024f, 0.671877f,
+        1.000004f, 0.671847f,
+        0.999958f, 0.336064f,
+        0.667979f, 0.335851f,
+        0.668104f, 0.000013f,
+        0.335973f, 0.335903f,
+        0.667979f, 0.335851f,
+        0.335973f, 0.335903f,
+        0.668104f, 0.000013f,
+        0.336098f, 0.000071f,
+        0.000103f, 0.336048f,
+        0.000004f, 0.671870f,
+        0.336024f, 0.671877f,
+        0.000103f, 0.336048f,
+        0.336024f, 0.671877f,
+        0.335973f, 0.335903f,
+        0.667969f, 0.671889f,
+        1.000004f, 0.671847f,
+        0.667979f, 0.335851f
     };
 
     GLuint uvbuffer;
@@ -184,9 +186,18 @@ int main(void)
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
     
+    // Cull triangles which normal is not towards the camera
+    glEnable(GL_CULL_FACE);
+    
     do{
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glUseProgram(programID);
+        
+        computeMatricesFromInputs();
+        glm::mat4 ProjectionMatrix = getProjectionMatrix();
+        glm::mat4 ViewMatrix = getViewMatrix();
+        glm::mat4 ModelMatrix = glm::mat4(1.0);
+        glm::mat4 mvp = ProjectionMatrix * ViewMatrix * ModelMatrix;
         
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
         
@@ -227,5 +238,7 @@ int main(void)
     glDeleteBuffers(1, &uvbuffer);
     glDeleteProgram(programID);
     glDeleteTextures(1, &TextureID);
+    
+    glfwTerminate();
 }
 
